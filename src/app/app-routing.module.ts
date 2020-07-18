06c686d7d41792b6f4976a922da51a3df2f523ee
arsam malik <arsam@hungryepos.com>
2020-07-18 20:05:25 +0500

    Header Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
-import {MainComponent} from "./main/main.component";
-import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from './header/header.component';
+import {MainComponent} from './main/main.component';
+import {FooterComponent} from './footer/footer.component';
 
 
 const routes: Routes = [
